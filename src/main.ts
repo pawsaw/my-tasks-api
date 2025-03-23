5244fb87d9cb78611d49966f6078b7b72ebe0dc8
Pawel Sawicki <pwlsawicki@gmail.com>
2025-03-23 11:42:15 +0100

    Adding dynamodb

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
-import { body, param, validationResult } from 'express-validator';
-import serverless from 'serverless-http';
+import express, { Request, Response } from "express";
+import { body, param, validationResult } from "express-validator";
+import serverless from "serverless-http";
 import {
   createTask,
   getAllTasks,
   getTaskById,
   updateTask,
   deleteTask,
-} from './taskService';
-import cors from 'cors';
+} from "./taskService";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
@@ -16,91 +16,112 @@ app.use(cors());
 
 // Validation Middleware
 const validateTask = [
-  body('title')
+  body("title")
     .isString()
     .isLength({ min: 3 })
-    .withMessage('Title must be at least 3 characters long'),
-  body('description')
+    .withMessage("Title must be at least 3 characters long"),
+  body("description")
     .isString()
     .isLength({ min: 5 })
-    .withMessage('Description must be at least 5 characters long'),
-  body('status')
-    .isIn(['To Do', 'In Progress', 'Completed'])
-    .withMessage('Invalid status'),
+    .withMessage("Description must be at least 5 characters long"),
+  body("status")
+    .isIn(["To Do", "In Progress", "Completed"])
+    .withMessage("Invalid status"),
 ];
 
 // Get all tasks
-app.get('/api/tasks', (req: Request, res: Response) => {
-  const tasks = getAllTasks();
+app.get("/api/tasks", async (req: Request, res: Response): Promise<void> => {
+  const tasks = await getAllTasks();
   res.json(tasks);
 });
 
 // Get task by ID
 app.get(
-  '/api/tasks/:id',
-  [param('id').isUUID()],
-  (req: Request, res: Response): void => {
+  "/api/tasks/:id",
+  [param("id").isUUID()],
+  async (req: Request, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
       return;
     }
-    const task = getTaskById(req.params.id);
-    if (!task) {
-      res.status(404).json({ message: 'Task not found' });
-      return;
+
+    try {
+      const task = await getTaskById(req.params.id);
+      if (!task) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+      res.json(task);
+    } catch (error) {
+      res.status(500).json(error);
     }
-    res.json(task);
   }
 );
 
 // Create a new task
-app.post('/api/tasks', validateTask, (req: Request, res: Response): void => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
+app.post(
+  "/api/tasks",
+  validateTask,
+  async (req: Request, res: Response): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const newTask = await createTask(req.body);
+      res.status(201).json(newTask);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
-  const newTask = createTask(req.body);
-  res.status(201).json(newTask);
-});
+);
 
 // Update an existing task
 app.put(
-  '/api/tasks/:id',
-  [param('id').isUUID(), ...validateTask],
-  (req: Request, res: Response): void => {
+  "/api/tasks/:id",
+  [param("id").isUUID(), ...validateTask],
+  async (req: Request, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
       return;
     }
-    const updatedTask = updateTask(req.params.id, req.body);
-    if (!updatedTask) {
-      res.status(404).json({ message: 'Task not found' });
-      return;
+
+    try {
+      const updatedTask = await updateTask(req.params.id, req.body);
+      if (!updatedTask) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+      res.status(200).json(updatedTask);
+    } catch (error) {
+      res.status(500).json(error);
     }
-    res.status(200).json(updatedTask);
   }
 );
 
 // Delete a task
 app.delete(
-  '/api/tasks/:id',
-  param('id').isUUID(),
-  (req: Request, res: Response): void => {
+  "/api/tasks/:id",
+  param("id").isUUID(),
+  async (req: Request, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
       return;
     }
-    const success = deleteTask(req.params.id);
-    if (!success) {
-      res.status(404).json({ message: 'Task not found' });
-      return;
+    try {
+      const success = await deleteTask(req.params.id);
+      if (!success) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).json(error);
     }
-    res.status(204).send();
   }
 );
-
 
 export const handler = serverless(app);
--- src/taskService.ts
+++ src/taskService.ts
@@ -1,45 +1,103 @@
 // taskService.ts
-import { v4 as uuidv4 } from 'uuid';
-import { Task } from './task';
+import { v4 as uuidv4 } from "uuid";
+import { Task } from "./task";
 
-const tasks: Task[] = [];
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  GetCommandInput,
+  PutCommand,
+  PutCommandInput,
+  ScanCommand,
+  ScanCommandInput,
+  DeleteCommand,
+  DeleteCommandInput,
+} from "@aws-sdk/lib-dynamodb";
+
+const TASKS_TABLE = process.env.TASKS_TABLE;
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
 
 // Create a new task
-export const createTask = (taskData: Omit<Task, 'id'>): Task => {
+export const createTask = async (taskData: Omit<Task, "id">): Promise<Task> => {
   const newTask = { id: uuidv4(), ...taskData };
-  tasks.unshift(newTask);
+  const cmdInput: PutCommandInput = {
+    TableName: TASKS_TABLE,
+    Item: newTask,
+  };
+  try {
+    const command = new PutCommand(cmdInput);
+    await docClient.send(command);
+  } catch (error) {
+    throw new Error("Failed to create task");
+  }
   return newTask;
 };
 
 // Get all tasks
-export const getAllTasks = (): Task[] => {
-  return tasks;
+export const getAllTasks = async (): Promise<Task[]> => {
+  const cmdInput: ScanCommandInput = {
+    TableName: TASKS_TABLE,
+  };
+  try {
+    const command = new ScanCommand(cmdInput);
+    const { Items } = await docClient.send(command);
+    return Items as Task[];
+  } catch (error) {
+    throw new Error("Failed to get tasks");
+  }
 };
 
 // Get task by ID
-export const getTaskById = (id: string): Task | undefined => {
-  return tasks.find((task) => task.id === id);
+export const getTaskById = async (id: string): Promise<Task | undefined> => {
+  const cmdInput: GetCommandInput = {
+    TableName: TASKS_TABLE,
+    Key: { id },
+  };
+  try {
+    const command = new GetCommand(cmdInput);
+    const { Item } = await docClient.send(command);
+    return Item as Task;
+  } catch (error) {
+    throw new Error("Failed to get task");
+  }
 };
 
 // Update an existing task
-export const updateTask = (
+export const updateTask = async (
   id: string,
   taskData: Partial<Task>
-): Task | undefined => {
-  const taskIndex = tasks.findIndex((task) => task.id === id);
-  if (taskIndex !== -1) {
-    tasks[taskIndex] = { ...tasks[taskIndex], ...taskData };
-    return tasks[taskIndex];
+): Promise<Task | undefined> => {
+  const task = await getTaskById(id);
+  if (!task) {
+    return undefined;
   }
-  return undefined;
+  const updatedTask = { ...task, ...taskData };
+  const cmdInput: PutCommandInput = {
+    TableName: TASKS_TABLE,
+    Item: updatedTask,
+  };
+  try {
+    const command = new PutCommand(cmdInput);
+    await docClient.send(command);
+  } catch (error) {
+    throw new Error("Failed to update task");
+  }
+  return updatedTask;
 };
 
 // Delete a task
-export const deleteTask = (id: string): boolean => {
-  const taskIndex = tasks.findIndex((task) => task.id === id);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
+export const deleteTask = async (id: string): Promise<boolean> => {
+  const cmdInput: DeleteCommandInput = {
+    TableName: TASKS_TABLE,
+    Key: { id },
+  };
+  try {
+    const command = new DeleteCommand(cmdInput);
+    await docClient.send(command);
     return true;
+  } catch (error) {
+    throw new Error("Failed to delete task");
   }
-  return false;
 };
